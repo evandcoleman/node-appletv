a310b5616a5322f36d35a63ca74ca7a5667074a7
SeppST <sebastian.staats@cheil.com>
2019-05-05 19:46:10 +0200

    Fixes

--- dist/lib/typed-events.d.ts
+++ dist/lib/typed-events.d.ts
@@ -3,6 +3,6 @@ import { EventEmitter } from 'events';
 export default class TypedEventEmitter<T> extends EventEmitter {
     constructor(...args: any[]);
     _on(event: string, callback: (...args) => void): this;
-    on(event: keyof T, callback: (arg: T[typeof event]) => void): this;
-    emit(event: keyof T, payload?: T[typeof event]): boolean;
+    on(event: string & keyof T, callback: (arg: T[typeof event]) => void): this;
+    emit(event: string & keyof T, payload?: T[typeof event]): boolean;
 }
--- src/lib/typed-events.ts
+++ src/lib/typed-events.ts
@@ -1,19 +1,19 @@
-import { EventEmitter } from 'events'
+import { EventEmitter } from 'events';
 
 export default class TypedEventEmitter<T> extends EventEmitter {
   constructor(...args) {
-    super()
+    super();
   }
 
   _on(event: string, callback: (...args) => void): this {
-    return super.on(event, callback)
+    return super.on(event, callback);
   }
 
-  on(event: keyof T, callback: (arg: T[typeof event]) => void): this {
-    return super.on(event, callback)
+  on(event: string & keyof T, callback: (arg: T[typeof event]) => void): this {
+    return super.on(event, callback);
   }
 
-  emit(event: keyof T, payload?: T[typeof event]): boolean {
-    return super.emit(event, payload)
+  emit(event: string & keyof T, payload?: T[typeof event]): boolean {
+    return super.emit(event, payload);
   }
 }
